6e49f538f1c9f735d86380beb3fe63556123ed90
Yohn <yohn@letstalk.nl>
2025-06-19 20:08:32 +0200

    feat: add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_pokemon_app/ui/screens/HomeScreen.dart';
-import 'package:shadcn_ui/shadcn_ui.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+import 'package:flutter_pokemon_app/services/pokemon_provider.dart';
+import 'package:flutter_pokemon_app/ui/screens/HomeScreen.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const ShadApp.material(home: Homescreen()));
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => PokemonProvider(),
+      child: const ShadApp.material(home: Homescreen()),
+    ),
+  );
 }
